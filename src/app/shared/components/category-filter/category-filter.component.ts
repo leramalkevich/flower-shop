96ab72204842ff4a9d7fc99bdda174c502456a3e
leramalkevich <leramalkevich@gmail.com>
2025-02-13 15:39:53 +0300

    created category filter

---
+++ src/app/shared/components/category-filter/category-filter.component.ts
@@ -0,0 +1,55 @@
+import {Component, Input} from '@angular/core';
+import {CategoryWithTypeType} from '../../../../types/category-with-type.type';
+import {Router} from '@angular/router';
+import {ActiveParamsType} from '../../../../types/activeParams.type';
+
+@Component({
+  selector: 'category-filter',
+  standalone: false,
+  templateUrl: './category-filter.component.html',
+  styleUrl: './category-filter.component.scss'
+})
+export class CategoryFilterComponent {
+  @Input() categoryWithTypes: CategoryWithTypeType | null = null;
+  @Input() type: string | null = null;
+  open = false;
+  activeParams: ActiveParamsType = {types: []};
+
+  get title(): string {
+    if (this.categoryWithTypes) {
+      return this.categoryWithTypes.name;
+    } else if (this.type) {
+      if (this.type === 'height') {
+        return 'Высота';
+      } else if (this.type === 'diameter') {
+        return 'Диаметр';
+      }
+    }
+    return '';
+  }
+
+  constructor(private router: Router) {
+  }
+
+  toggle(): void {
+    this.open = !this.open;
+  }
+
+  updateFilterParam(url: string, checked: boolean) {
+    if (this.activeParams.types && this.activeParams.types.length > 0) {
+      const existingTypeInParams = this.activeParams.types.find(item => item === url);
+      if (existingTypeInParams && !checked) {
+        this.activeParams.types = this.activeParams.types.filter(item => item !== url);
+      } else if (!existingTypeInParams && checked) {
+        this.activeParams.types.push(url);
+      }
+    } else if (checked) {
+      this.activeParams.types = [url];
+    }
+
+    this.router.navigate(['/catalog'], {
+      queryParams: this.activeParams
+    });
+  }
+
+}
--- src/app/views/product/catalog/catalog.component.ts
+++ src/app/views/product/catalog/catalog.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../../../shared/services/product.service';
 import {ProductType} from '../../../../types/product.type';
+import {CategoryService} from '../../../shared/services/category.service';
+import {CategoryWithTypeType} from '../../../../types/category-with-type.type';
 
 @Component({
   selector: 'app-catalog',
@@ -10,14 +12,21 @@ import {ProductType} from '../../../../types/product.type';
 })
 export class CatalogComponent implements OnInit {
   products: ProductType[] = [];
+  categoriesWithTypes:CategoryWithTypeType[]=[];
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService, private categoryService:CategoryService) {
   }
 
   ngOnInit(): void {
     this.productService.getProducts()
       .subscribe(data => {
         this.products = data.items;
+      });
+
+    this.categoryService.getCategoriesWithTypes()
+      .subscribe(data =>{
+        console.log(data);
+        this.categoriesWithTypes = data;
       })
   }
 
