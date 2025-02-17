33527da86b08ef0bbf3d6f5c249ad9f4b6282d8c
leramalkevich <leramalkevich@gmail.com>
2025-02-17 23:13:13 +0300

    created making order

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -9,10 +9,16 @@ import {DefaultResponseType} from '../../../types/default-response.type';
   providedIn: 'root'
 })
 export class CartService {
-  count: number = 0;
+  private count: number = 0;
   count$: Subject<number> = new Subject<number>();
 
   constructor(private http: HttpClient) {
+  }
+
+  setCount(count:number){
+    this.count = count;
+    // оповещаем всех слушателей, что count изменился
+    this.count$.next(this.count);
   }
 
   getCart(): Observable<CartType | DefaultResponseType> {
@@ -24,8 +30,7 @@ export class CartService {
       .pipe(
         tap(data => {
           if (!data.hasOwnProperty('error')) {
-            this.count = (data as { count: number }).count;
-            this.count$.next(this.count);
+            this.setCount((data as { count: number }).count);
           }
         })
       );
@@ -36,11 +41,12 @@ export class CartService {
       .pipe(
         tap(data => {
           if (!data.hasOwnProperty('error')) {
-            this.count = 0;
+            let count = 0;
             (data as CartType).items.forEach(item => {
-              this.count += item.quantity;
+              count += item.quantity;
             });
-            this.count$.next(this.count);
+
+            this.setCount(count);
           }
         })
       );
--- src/app/views/order/order.module.ts
+++ src/app/views/order/order.module.ts
@@ -6,6 +6,8 @@ import { CartComponent } from './cart/cart.component';
 import { OrderComponent } from './order/order.component';
 import {SharedModule} from '../../shared/shared.module';
 import {CarouselModule} from 'ngx-owl-carousel-o';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @NgModule({
@@ -17,6 +19,8 @@ import {CarouselModule} from 'ngx-owl-carousel-o';
     CommonModule,
     SharedModule,
     CarouselModule,
+    ReactiveFormsModule,
+    MatDialogModule,
     OrderRoutingModule
   ]
 })
