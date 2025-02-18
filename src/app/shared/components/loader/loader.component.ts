edf89a91d3efcdbb3232aa812dd9ccc65df6e0d7
leramalkevich <leramalkevich@gmail.com>
2025-02-18 17:00:44 +0300

    created loader and fixed links

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {LoaderService} from '../../services/loader.service';
+
+@Component({
+  selector: 'app-loader',
+  standalone: false,
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss'
+})
+export class LoaderComponent implements OnInit {
+
+  isShowed: boolean = false;
+
+  constructor(private loaderService: LoaderService) {
+  }
+
+  ngOnInit(): void {
+    this.loaderService.isShowed$.subscribe((isShowed: boolean) => {
+      this.isShowed = isShowed;
+    });
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,16 +6,19 @@ import {RouterModule} from "@angular/router";
 import {FormsModule} from '@angular/forms';
 import { CategoryFilterComponent } from './components/category-filter/category-filter.component';
 import { CountSelectorComponent } from './components/count-selector/count-selector.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 @NgModule({
-  declarations: [PasswordRepeatDirective, ProductCardComponent, CategoryFilterComponent, CountSelectorComponent],
+  declarations: [PasswordRepeatDirective, ProductCardComponent, CategoryFilterComponent, CountSelectorComponent, LoaderComponent],
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    MatProgressSpinnerModule
   ],
-  exports: [PasswordRepeatDirective, ProductCardComponent, CategoryFilterComponent, CountSelectorComponent]
+  exports: [PasswordRepeatDirective, ProductCardComponent, CategoryFilterComponent, CountSelectorComponent, LoaderComponent]
 })
 export class SharedModule {
 }
