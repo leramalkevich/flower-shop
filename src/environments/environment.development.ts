24b0c584045363ed7e354410b4709254f82f2b00
leramalkevich <leramalkevich@gmail.com>
2025-10-16 14:29:44 +0300

    added online environmemt api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  api:"http://localhost:3000/api/",
-  serverStaticPath: "http://localhost:3000/images/products/"
+  // api:"http://localhost:3000/api/",
+  api:"https://flowershop-hrrw.onrender.com/api/",
+  serverStaticPath: "https://flowershop-hrrw.onrender.com/images/products/"
+  // serverStaticPath: "http://localhost:3000/images/products/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  api:"http://localhost:3000/api/",
-  serverStaticPath: "http://localhost:3000/images/products/"
+  // api:"http://localhost:3000/api/",
+  // serverStaticPath: "http://localhost:3000/images/products/",
+  api:"https://flowershop-hrrw.onrender.com/api/",
+  serverStaticPath: "https://flowershop-hrrw.onrender.com/images/products/"
 };
